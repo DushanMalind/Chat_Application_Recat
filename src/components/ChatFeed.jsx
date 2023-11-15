ad70e87529eba6fd3fc6fbee3f07ea8133ec72e4
DushanMalind <dushanmalinda010@gmail.com>
2023-11-15 21:43:18 +0530

    message block

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -3,7 +3,37 @@ import MyMessage from "./MyMessage";
 import TheirMessage from "./TheirMessage";
 
 const ChatFeed =(props) =>{
- const {chats, activChat, userName, message}=props;
+ const {chats, activChat, userName, messages}=props;
+
+ const chat=chats && chats[activChat];
+
+//  console.log(chat, userName, messages);
+
+const renderMessages =() =>{
+    const keys=Object.keys(messages);
+
+    // console.log(keys);
+
+    return keys.map((key, index) =>{
+        const message=messages[key];
+        const lastMessageKey=index === 0 ? null : keys[index - 1];
+        const isMyMessage=userName === message.sender.username;
+
+        return(
+            <div key={`msg_${index}`} style={{width:'100%'}}>
+                <div className="message-block">
+                    {
+                        isMyMessage
+                        ? <MyMessage />
+                        : <TheirMessage />
+                    }
+                </div>
+            </div>
+        )
+    })
+ }
+
+renderMessages();
 
 return (
     <div>
