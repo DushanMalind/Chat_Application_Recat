195c9659e091b821f578fb186f86fcd72c3f608d
DushanMalind <dushanmalinda010@gmail.com>
2023-11-15 22:04:31 +0530

    MyMessage

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,4 +1,14 @@
-const MyMessage =() =>{
+const MyMessage =({message}) =>{
+    if(message?.attachments?.length > 0){
+        return(
+            <img
+                src={message.attachments[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{float:'right'}}
+            />
+        )
+    }
     return (
         <div>
             MyMessage 
