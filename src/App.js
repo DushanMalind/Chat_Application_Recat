2341d39c721d85c5f383d46719e5f62c9166dec1
DushanMalind <dushanmalinda010@gmail.com>
2023-11-15 21:16:00 +0530

    function add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
 import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="4d0c3452-1941-467e-b829-105d690e1796"
+            userName="Dushan"
+            userSecret="123456"
+            rendarChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    );
+    
+}
+export default App;
