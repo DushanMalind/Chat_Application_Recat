b1d4fc7f60dc4e493df438b02b82ad12573ccaff
DushanMalind <dushanmalinda010@gmail.com>
2023-11-28 12:17:59 +0530

    TheirMessage retun ok

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,30 @@
 const TheirMessage =({lastMessage,messsage}) =>{
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
     return (
-        <div>
-            TheirMessage  
+        <div className="message-row">
+            {isFirstMessageByUser &&(
+                <div
+                   className="message-avatar"
+                   style={{backgroundImage: `url(${message?.sender?.avatar})`} }
+                
+                />
+            )}  
+            {
+                message?.attachments?.length > 0
+                    ? (
+                        <img
+                            src={message.attachments[0].file}
+                            alt="message-attachment"
+                            className="message-image"
+                            style={{float:'right'}}
+                        />
+                    ) :(
+                        <div className="message" style={{float:'right', marginRight: '18px', color:'white', backgroundColor:'#3B2A50'}}>
+                        {message.text}
+                   </div>
+                    )
+                }
+            
         </div>
     );
 }
